b4207f03d6b24cfb84a452bf8313800438943a33
CHOMPOO (ชมพู่) <88492141+DuangkamonPhobsungnoen@users.noreply.github.com>
2023-03-21 18:52:14 +0700

    add Recommend on Netflix

--- vue-project/src/components/Home/MiniReviewCard.vue
+++ vue-project/src/components/Home/MiniReviewCard.vue
@@ -7,7 +7,7 @@ export default {
 <template>
     <div class="task-container columns is-multiline px-6">
         <div class="column is-4 p-6" v-for="i in 3" :key="i">
-            <div class="card " style="border-radius: 35px; background-color: #452676; color: white;">
+            <div class="card color-background-purple-2 has-text-white" style="border-radius: 35px;">
                 <div class="card-image ">
                     <figure class="image is-4by3">
                         <img src="https://media.discordapp.net/attachments/1087447051387813909/1087667903056318544/poster_1.png?width=310&height=221" alt="Placeholder image">
--- vue-project/src/components/Home/Recommend.vue
+++ vue-project/src/components/Home/Recommend.vue
@@ -5,17 +5,38 @@ export default {
 </script>
 
 <template>
-  <div class="card">
-  <div class="card-content">
-    <div class="content">
-      Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
+    <div class="px-6 pb-6">
+        <div class="card has-text-white color-background-purple-2 p-5" style="border-radius: 25px;">
+            <div class="card-content">
+                <div class="title has-text-white ">Recommend on Netflix</div>
+                <div class="columns ">
+                    <div class="column is-6">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/dVWHs5b5LCs"
+                            title="YouTube video player" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowfullscreen></iframe>
+                    </div>
+                    <div class="column">
+                        <div class="content">
+                            <div class="is-size-3">
+                                <b>Alchemy of Souls</b>
+                            </div>
+                            <div class="is-size-5">
+                                A powerful sorceress in a blind woman's body encounters a man from a prestigious family, who
+                                wants her help to change his destiny.
+                                <br><br>
+                                Starring:Lee Jae-wook, Go Youn-jung, Hwang Min-hyun
+                                Creators:Park Joon-hwa, Hong Jung-eun, Hong Mi-ran
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
-  </div>
-</div>
 </template>
 
 
 
-<style>
-
-</style>
+<style></style>
