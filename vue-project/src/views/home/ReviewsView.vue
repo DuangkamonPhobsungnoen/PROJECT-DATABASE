c6ec2dc8ea540971ce0e92d7922a81399d015ed7
CHOMPOO (ชมพู่) <88492141+DuangkamonPhobsungnoen@users.noreply.github.com>
2023-03-26 01:56:25 +0700

    add ReviewView

--- vue-project/src/views/home/ReviewsView.vue
+++ vue-project/src/views/home/ReviewsView.vue
@@ -2,5 +2,46 @@
 </script>
 
 <template>
-    <h1 class="is-size-1">ReviewView</h1>
+  <div class="has-text-centered p-6"><b class="is-size-3">REVIEWS</b></div>
+  <div class="p-3" v-for="i in 3" :key="i">
+    <div
+      class="card has-text-white color-background-purple-2 column is-10 is-offset-1"
+      style="border-radius: 25px; border: 1px solid white;" 
+    >
+      <div class="p-3">
+        <div class="columns">
+          <div class="column is-3">
+            <figure>
+              <img 
+                src="https://media.discordapp.net/attachments/1087447051387813909/1089255404921434122/535881e220a3792a1640a2e66e6c79b4_1.png?width=270&height=362" width="150" height="150"
+                alt="Placeholder image"
+              />
+            </figure>
+          </div>
+          <div class="column">
+            <div class="content">
+              <div class="media">
+                <div class="media-content">
+                  <p class="is-size-4">The Grand Budapest Hotel</p>
+                  <div class="text_img_center ">
+                    <img
+                      src="https://media.discordapp.net/attachments/1087447051387813909/1087616553777897552/Star.png?width=20&height=20"
+                    />
+                    <span class="pl-2">9.5</span>
+                  </div>
+                </div>
+              </div>
+              <div>
+                สนุกเว่อแบบตัวแม่ ภาพสวยสับปังๆๆ นักแสดงแสดงดีมากรางวัลออสการ์
+                ต้องเข้าสักตัวแล้ว นักแสดงแสดงดีมากรางวัลออสการ์ นักแสดงแสดงดีมากรางวัลออสการ์ นักแสดงแสดงดีมากรางวัลออสการ์ รางวัลออสการ์ดูแล้วอยากดำน้ำดูปะการั..
+                <p class="text_img_center pt-5">
+                  <span>dkm_ch</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</div>
 </template>
