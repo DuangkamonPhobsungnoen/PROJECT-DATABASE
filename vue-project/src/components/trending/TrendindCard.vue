4d0b3b6053c6a42bbd1c172aa2cd9628d8dd90b7
CHOMPOO (ชมพู่) <88492141+DuangkamonPhobsungnoen@users.noreply.github.com>
2023-03-25 21:50:36 +0700

    add card in TrendingView

--- vue-project/src/components/trending/TrendindCard.vue
+++ vue-project/src/components/trending/TrendindCard.vue
@@ -6,10 +6,11 @@ export default {
 
 <template>
     <div class="task-container columns is-multiline px-6">
-        <div class="column is-4 p-6" v-for="i in 3" :key="i">
+        <div class="column is-4 p-5" v-for="i in 6" :key="i">
+            <div class="is-size-2 has-text-centered pb-6" style="font-family: 'Bungee Inline', cursive;">{{ i }}</div>
             <div class="card color-background-purple-2 has-text-white" style="border-radius: 35px;">
-                <div class="card-image ">
-                    <figure class="image is-4by3">
+                <div class="card-image  ">
+                    <figure class="image is-square ">
                         <img src="https://media.discordapp.net/attachments/1087447051387813909/1087667903056318544/poster_1.png?width=310&height=221" alt="Placeholder image">
                     </figure>
                 </div>
@@ -22,16 +23,6 @@ export default {
                             <span class="pl-2">9.5</span>
                         </p>
                         </div>
-                    </div>
-
-                    <div class="content">
-                        สนุกตื่นเต้นเร้าใจว้าวซ่า ภาพสวย
-                        มากเหมือนหลุดมาจากในหนัง
-                        ตอนแรกนึกว่าเป็นแนวโรแมน...
-                        <p class="text_img_center pt-5">
-                            <img src="https://media.discordapp.net/attachments/1087447051387813909/1087617962984357918/Ellipse_7.png?width=43&height=43 ">
-                            <span class="pl-2">dkm_ch</span>
-                        </p>
                     </div>
                 </div>
             </div>
--- vue-project/src/views/home/TrendingView.vue
+++ vue-project/src/views/home/TrendingView.vue
@@ -1,7 +1,10 @@
 <script setup>
 import SearchMovie from '../../components/SearchMovie.vue';
+import TrendindCardVue from '../../components/trending/TrendindCard.vue';
 </script>
 
 <template>
     <SearchMovie></SearchMovie>
+    <div class="has-text-centered p-6"><b class="is-size-3 ">TRENDING</b></div>
+    <TrendindCardVue></TrendindCardVue>
 </template>
