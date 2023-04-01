7bcba194eceefb813d363876d0a4f430ec88cd9e
Jeerawat Maiwai <102474034+JeerawatM@users.noreply.github.com>
2023-04-02 01:16:22 +0700

    popup view ยังหาทางแก้อยู่แต่ใช้ได้แล้ว

--- vue-project/src/components/PopUpReview.vue
+++ vue-project/src/components/PopUpReview.vue
@@ -1,13 +1,63 @@
+
 <template>
-  
+<!-- โยนข้อมูลข้ามcomponent ไม่เป็น -->
+  <div class="modal" style="background: hsla(0, 0%, 0%, 70%)" @click="show = !show" v-bind:class="{ 'is-active': show }">
+    <div class="p-3">
+    <div class="card has-text-white color-background-purple-2 column is-8 is-offset-2" style="border-radius: 25px; border: 1px solid white">
+      <div class="p-6">
+        <div class="columns">
+          <div class="column is-3">
+            <figure>
+              <img
+                src="https://media.discordapp.net/attachments/1087447051387813909/1089255404921434122/535881e220a3792a1640a2e66e6c79b4_1.png?width=270&height=362"
+                width="400"
+                height="400"
+                alt="Placeholder image"
+              />
+            </figure>
+          </div>
+          <div class="column">
+            <div class="content">
+              <div class="media">
+                <div class="media-content">
+                  <p class="is-size-4">The Grand Budapest Hotel</p>
+                  <div class="text_img_center">
+                    <img
+                      src="https://media.discordapp.net/attachments/1087447051387813909/1087616553777897552/Star.png?width=20&height=20"
+                    />
+                    <span class="pl-2">9.5</span>
+                  </div>
+                </div>
+              </div>
+              <div>
+                สนุกเว่อแบบตัวแม่ ภาพสวยสับปังๆๆ นักแสดงแสดงดีมากรางวัลออสการ์
+                ต้องเข้าสักตัวแล้ว นักแสดงแสดงดีมากรางวัลออสการ์
+                นักแสดงแสดงดีมากรางวัลออสการ์ นักแสดงแสดงดีมากรางวัลออสการ์
+                รางวัลออสการ์ดูแล้วอยากดำน้ำดูปะการั..
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="has-text-right" style="position:relative; bottom:20px;">
+            <span class="pr-5 subtitle is-5 has-text-white">dkm_ch</span>
+            <img src="https://cdn.discordapp.com/attachments/1087447051387813909/1087617962984357918/Ellipse_7.png" width="50" height="70" alt="" class="mr-6">
+        </div>
+    </div>
+  </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: "profile",
+  data() {
+    return {
+      show: false,
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
