053881bc5b1a90621bac39111edf56e8aeb6b76e
FixFire <89625074+NatthawutSK@users.noreply.github.com>
2023-04-04 17:13:49 +0700

    add everything jingabel

--- vue-project/src/views/SigninView.vue
+++ vue-project/src/views/SigninView.vue
@@ -1,3 +1,9 @@
+<script setup>
+import { useSignInStore } from "../stores/signin";
+const signInStore  = useSignInStore()
+
+</script>
+
 <template>
     <div class="hero is-fullheight">
         <div class="hero-body is-justify-content-center is-align-items-center">
@@ -5,16 +11,16 @@
                 <div class="columns is-flex is-flex-direction-column card1">
                     <h1 class="text-signin">SIGN IN</h1>
                     <div class="column px-5">
-                        <input v-model="username" class="input input_username" type="text" placeholder="Username" :class="{'is-danger': error.username}" @input="validateUsername()">
-                        <span class="has-text-danger ertext ml-3">{{error.username}}</span>
+                        <input v-model="signInStore.loginData.username" class="input input_username" type="text" placeholder="Username" :class="{'is-danger': signInStore.error.username}" @input="signInStore.validateUsername()">
+                        <span class="has-text-danger ertext ml-3">{{signInStore.error.username}}</span>
                     </div>
                     <div class="column px-5">
-                        <input v-model="passw" class="input input_password" type="password" placeholder="Password" :class="{'is-danger': error.passw}" @input="validatePassw()">
-                        <span class="has-text-danger ertext ml-3">{{error.passw}}</span>
+                        <input v-model="signInStore.loginData.password" class="input input_password" type="password" placeholder="Password" :class="{'is-danger': signInStore.error.passw}" @input="signInStore.validatePassw()">
+                        <span class="has-text-danger ertext ml-3">{{signInStore.error.passw}}</span>
                     </div>
                     <div class="column px-5">
                         <a>
-                            <button @click="cusname()" class="button is-fullwidth color_sign_purple " type="submit">Sign in</button>
+                            <button @click="signInStore.cusname()" class="button is-fullwidth color_sign_purple " type="submit">Sign in</button>
                         </a>
                     </div>
                     <div class="column px-5">
@@ -29,49 +35,4 @@
     </div>
 </template>
 
-<script>
-export default {
-  name: 'valid_signin',
-  data() {
-    return {
-        username: '',
-        passw: '',
-        error: {
-            username: '',
-            passw: '',
-        }
-    }
-  },
-  methods: {
-        validateUsername() {
-            if (this.username === '') {
-                this.error.username = 'กรุณากรอกชื่อผู้ใช้'
-                return
-            }
-            this.error.username = ''
-        },
-        validatePassw() {
-            if (this.passw === '') {
-                this.error.passw = 'กรุณากรอกรหัสผ่าน'
-                return
-            }
 
-            if (this.passw.length < 6) {
-                this.error.passw = 'รหัสผ่านห้ามน้อยกว่า 6 ตัวอักษร'
-                return
-            }
-            this.error.passw = ''
-        },
-        cusname() {
-            this.validateUsername()
-            this.validatePassw()
-            if (this.error.username !== '' || this.error.passw !== '') {
-                alert('กรุณากรอกข้อมูลให้ถูกต้อง')
-                return
-            }
-            window.location.href = '/'
-        }
-
-    },
-}
-</script>
