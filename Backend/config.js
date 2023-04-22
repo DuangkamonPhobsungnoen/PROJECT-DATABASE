0c222fc075ad9a7350ef4165046d7e380c036688
Tadporn <64070153@kmitl.ac.th>
2023-04-22 14:04:43 +0700

    .

--- Backend/config.js
+++ Backend/config.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: '022546Ochi',
   database: 'real_filmary',
   waitForConnections: true,
   connectionLimit: 10,
